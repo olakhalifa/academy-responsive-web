6b932c852be916dd29b137ff3aa7ef2a99c899fe
Ola Khalifa <ola.ahmed@live.co.uk>
2018-01-15 16:54:43 +1300

    Better SCSS use

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var app = new Vue({
   el: '.content',
 
   data: {
-    movies: []
+    movies: {}
   },
 
   created: function() {
@@ -13,11 +13,30 @@ var app = new Vue({
     fetchData: function() {
       var self = this;
       var list = '/api/';
+      var votes = '/api/votes';
 
       fetch(list).then(function(response) {
         return response.json();
       }).then(function(json) {
-        self.movies = json.movies;
+        var movies = {}
+
+        json.movies.forEach(function(movie){
+          movie.votes = 0
+          movies[movie.id] = movie
+        });
+
+        self.movies = movies;
+
+        fetch(votes).then(function(voteResponse) {
+          return voteResponse.json();
+        }).then(function(voteJson) {
+          voteJson.votes.forEach(function(vote) {
+            var id = vote.movie.id;
+            var movie = self.movies[id];
+
+            Vue.set(movie, 'votes', movie.votes + 1);
+          });
+        });
       });
     }
   }
